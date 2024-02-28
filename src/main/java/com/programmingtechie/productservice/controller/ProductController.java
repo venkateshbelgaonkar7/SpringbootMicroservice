7fc854679934da3f5f115bcc70a8900bbbb55c7f
venkateshbelgaonkar7 <Baloney@1234567>
2024-02-28 18:52:44 +0530

    updated with new docker files and connected to mongodb

--- src/main/java/com/programmingtechie/productservice/controller/ProductController.java
+++ src/main/java/com/programmingtechie/productservice/controller/ProductController.java
@@ -4,7 +4,6 @@ import com.programmingtechie.productservice.service.ProductService;
 import com.programmingtechie.productservice.dto.ProductRequest;
 import com.programmingtechie.productservice.dto.ProductRespose;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
