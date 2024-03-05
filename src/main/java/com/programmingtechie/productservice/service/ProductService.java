c9274e2bbfc8e08995a344d4208374a35d6cde34
venkateshbelgaonkar7 <venkateshbelgaonkar777@gmail.com>
2024-03-05 10:29:42 +0530

    properties changed

--- src/main/java/com/programmingtechie/productservice/service/ProductService.java
+++ src/main/java/com/programmingtechie/productservice/service/ProductService.java
@@ -4,35 +4,35 @@ import com.programmingtechie.productservice.dto.ProductRequest;
 import com.programmingtechie.productservice.dto.ProductRespose;
 import com.programmingtechie.productservice.model.Product;
 import com.programmingtechie.productservice.repository.ProductRepository;
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@RequiredArgsConstructor
 @Service
+@AllArgsConstructor
 @Slf4j
 public class ProductService {
-    private final ProductRepository productRepository;
 
+    private final ProductRepository productRepository;
     public void createProduct(ProductRequest productRequest) {
         Product product = Product.builder()
                 .name(productRequest.getName())
-                .description(productRequest.getDescription())
+                .description(productRequest.getName())
                 .price(productRequest.getPrice())
                 .build();
 
-        productRepository.save(product);
-        log.info("Product {} is saved",product.getId());
+            productRepository.save(product);
+            log.info("Product {} is saved ", product.getId());
     }
-
     public List<ProductRespose> getAllProducts() {
         List<Product> products = productRepository.findAll();
-        return products.stream().map(this::mapToProductRespose).toList();
+        return products.stream().map(this::mapTopProductResponse).toList();
     }
 
-    private ProductRespose mapToProductRespose(Product product) {
+    private ProductRespose mapTopProductResponse(Product product) {
         return ProductRespose.builder()
                 .id(product.getId())
                 .name(product.getName())
